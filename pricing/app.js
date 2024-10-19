57089665d4d9997a1db09cfb97aaeb7c37ec00e3
kh4fin <abdul18khafid@gmail.com>
2024-10-19 15:16:48 +0700

    modify: add ezgarage card and responsive to pricing

--- pricing/app.js
+++ pricing/app.js
@@ -1,20 +1,26 @@
 const slider = document.querySelector(".btn-slider");
 const sliderCircle = document.querySelector(".slider-circle");
-const parkEZContent = document.querySelector(".parkez-content");
-const ezGarageContent = document.querySelector(".ezgarage-content");
+const parkEZContent = document.querySelector(".thumb-parkez");
+const ezGarageContent = document.querySelector(".thumb-ezgarage");
+const ezgarage = document.getElementById("ezgarage");
+const parkez = document.getElementById("parkez");
 
 slider.addEventListener("click", function () {
   if (sliderCircle.style.left === "0px" || sliderCircle.style.left === "") {
     // EZGARAGE
-    sliderCircle.style.left = "90px";
-    sliderCircle.style.width = "120px";
+    sliderCircle.style.left = "80px";
+    sliderCircle.style.width = "130px";
     parkEZContent.style.display = "none";
     ezGarageContent.style.display = "block";
+    ezgarage.style.display = "block";
+    parkez.style.display = "none";
   } else {
     // parkEZ
     sliderCircle.style.left = "0px";
     sliderCircle.style.width = "90px";
     parkEZContent.style.display = "block";
     ezGarageContent.style.display = "none";
+    ezgarage.style.display = "none";
+    parkez.style.display = "block";
   }
 });
